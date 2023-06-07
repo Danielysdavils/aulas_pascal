f90bc34a4545c644f974499adaaff9f3192996c8
danielys <danielisdavils@gmail.com>
2023-06-07 01:28:40 -0300

    add exercicios

--- exlivro/Ex06/ex01.pas
+++ exlivro/Ex06/ex01.pas
@@ -1,24 +1,27 @@
+(* Faça uma função em Pascal que receba como parâmetro um número inteiro e
+teste se ele é um número binário. Se ele for binário, imprima sim senão imprima
+nao. *)
+
 program ex01;
 
 var n : longint;
 
 function ehBinario(n : longint) : boolean;
 var testeBin : boolean;
-    nMod : longint;
+    nMod: longint;
 
 begin
-	testeBin:= true;
-	while testeBin do
+	testeBin := true;
+	while (n <> 0) and (testeBin) do
 	begin
 		nMod:= n mod 10;
-		if (nMod <> 1) or (nMod <> 0) then
+		if (nMod <> 1) and (nMod <> 0) then
 			testeBin := false
 		else
 			n:= n div 10;
 	end;
 	ehBinario := testeBin;
 end;	
-
 begin
 	readln(n);
 	if ehBinario(n) then
